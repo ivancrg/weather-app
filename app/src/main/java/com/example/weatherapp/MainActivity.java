725c8f68dd9c64cd4ca62ba7520aabfda00f2cb9
irubi516 <ivan.rubinic@outlook.com>
2020-07-13 13:40:22 +0200

    Izbrisani komentari, ready to merge

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -10,27 +10,10 @@ import androidx.fragment.app.Fragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    //NAVIGATION VIEW private DrawerLayout drawerLayout;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*
-        NAVIGATION VIEW
-
-        Toolbar toolbar = findViewById(R.id.navigation_toolbar);
-        setSupportActionBar(toolbar);
-
-        drawerLayout = findViewById(R.id.navigation_drawer_layout);
-        NavigationView navigationView = findViewById(R.id.navigation_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();*/
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
@@ -48,40 +31,6 @@ public class MainActivity extends AppCompatActivity {
             }, 2500);
         }
     }
-
-    /*
-    NAVIGATION VIEW
-
-    @Override
-    public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        switch (menuItem.getItemId()) {
-            case R.id.navigation_main:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MainScreen()).commit();
-                break;
-            case R.id.navigation_forecastweek:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ForecastWeekScreen()).commit();
-                break;
-            case R.id.navigation_forecast48:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Forecast48Screen()).commit();
-                break;
-            case R.id.navigation_search:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SearchScreen()).commit();
-                break;
-        }
-
-        drawerLayout.closeDrawer(GravityCompat.START);
-
-        return true;
-    }*/
 
     private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
             menuItem -> {
